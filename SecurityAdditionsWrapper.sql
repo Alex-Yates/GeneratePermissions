90465023220dc51dfa3bb57a532170e0917b33cd
Alex Yates <alex.yates@dlmconsultants.com>
2020-02-20 17:48:57 +0000

    original version of code - carbon copied from Peter Schott's blog. (See Readme.md)

---
+++ SecurityAdditionsWrapper.sql
@@ -0,0 +1,31 @@
+﻿-- =============================================
+-- Pushes security based on the current environment variable setting
+-- =============================================
+
+PRINT '******************************* Deplying Permissions for DeployType = $(DeployType) ************************************'
+--:on error ignore
+
+IF ( '$(DeployType)' = 'Dev')
+BEGIN
+ :r .\SecurityAdditionsDEV.sql
+END
+ELSE IF ( '$(DeployType)' = 'QA')
+BEGIN
+ :r .\SecurityAdditionsQA.sql
+END
+ELSE IF ( '$(DeployType)' = 'Production')
+BEGIN
+ :r .\SecurityAdditionsProduction.sql
+END
+ELSE IF ( '$(DeployType)' = 'Staging')
+BEGIN
+ :r .\SecurityAdditionsStaging.sql
+END
+ELSE
+ELSE IF ( '$(DeployType)' = 'Local')
+BEGIN
+ :r .\SecurityAdditionsLocal.sql
+END
+BEGIN
+ :r .\SecurityAdditionsDefault.sql
+END
